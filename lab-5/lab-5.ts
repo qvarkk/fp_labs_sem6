aebb909f61123bf15110131e72275420d9c34bfe
Ilya <xumdes@gmail.com>
2025-04-05 09:48:40 +0500

    lab-5 done

--- lab-5/lab-5.ts
+++ lab-5/lab-5.ts
@@ -1,13 +1,24 @@
-const cartesianProduct = <T, U>(list1: T[], list2: U[]): [T, U][] => {
-  const result: [T, U][] = [];
-
-  for (const item1 of list1) {
-    for (const item2 of list2) {
-      result.push([item1, item2]);
-    }
-  }
-
-  Array.from(list1.length * list2.length);
-
-  return result;
+const formPairs = <T, U>(
+  list: T[],
+  el: U,
+  res: [T, U][] = []
+): [T | undefined, U][] => {
+  return list.length > 0
+    ? formPairs(list.slice(0, -1), el, [...res, [list[list.length - 1], el]])
+    : res;
 };
+
+const cartesianProduct = <T, U>(
+  list1: T[],
+  list2: U[],
+  res: [T, U][] = []
+): [T | undefined, U | undefined][] => {
+  return list2.length > 0
+    ? cartesianProduct(list1, list2.slice(0, -1), [
+        ...res,
+        ...formPairs(list1, list2[list2.length - 1]),
+      ])
+    : res;
+};
+
+console.log(cartesianProduct(["1", 2, 0x3], [4, "hello", "world"]));
